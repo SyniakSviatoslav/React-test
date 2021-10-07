b40702894933c491fe1e6e20cf8d84d66bbc763a
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2021-10-07 19:38:40 +0300

    started react

---
+++ react-start/src/app/app.js
@@ -0,0 +1,15 @@
+
+import './styles.css';
+
+function App() {
+  return (
+   <div className="container">
+     WOW React
+     <div className="wrapper">
+       New possibilities
+     </div>
+   </div>
+  
+  );
+}
+export default App;
