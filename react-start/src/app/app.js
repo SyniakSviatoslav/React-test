b191784b1a344cc94eedf111af5f250253be4263
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2021-10-08 21:10:28 +0300

    finished react tutorial

--- react-start/src/app/app.js
+++ react-start/src/app/app.js
@@ -1,15 +1,6 @@
 
 import './styles.css';
 
-function App() {
-  return (
-   <div className="container">
-     WOW React
-     <div className="wrapper">
-       New possibilities
-     </div>
-   </div>
-  
-  );
-}
-export default App;
+
+   
+
